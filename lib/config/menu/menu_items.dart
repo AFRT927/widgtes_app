bf5087a59bfbd5e71b5778c45209a3e73805c485
Andres <andrese-rios@utp.edu.co>
2023-07-20 08:34:54 -0500

    creacion de la pantalla de animaciones

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -21,6 +21,6 @@ const appMenuItems = <MenuItem>[
   MenuItem(title: 'Botones', subTitle: 'Varios botones en Flutter', link: '/buttons', icon: Icons.smart_button),
   MenuItem(title: 'Tarjetas', subTitle: 'Un contenedor Estilizado', link: '/cards', icon: Icons.credit_card),
   MenuItem(title: 'Progress Indicators', subTitle: 'Generales y controlados', link: '/progress', icon: Icons.refresh_rounded),
-  MenuItem(title: 'SnackBars y dialogos', subTitle: 'Indicadores en pantalla', link: '/snackbars', icon: Icons.info_outline)
-
+  MenuItem(title: 'SnackBars y dialogos', subTitle: 'Indicadores en pantalla', link: '/snackbars', icon: Icons.info_outline),
+  MenuItem(title: 'Animated Container', subTitle: 'Stateful widget animado', link: '/animated', icon: Icons.check_box_outlined),
 ];
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -35,6 +35,12 @@ final appRouter = GoRouter(
       path: '/snackbars',
       name:SnackBarScreen.name,
       builder: (context, state) => const SnackBarScreen(),
+      ),
+
+      GoRoute(
+      path: '/animated',
+      name:AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen(),
       )
 
   ]
