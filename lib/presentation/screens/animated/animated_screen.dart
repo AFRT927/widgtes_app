ebd5cc9ea49239b71c2e8502239ff1d0ee84a20d
Andres <andrese-rios@utp.edu.co>
2023-07-20 09:26:12 -0500

    configuracion del widget <AnimatedContainer>

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,9 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+class AnimatedScreen extends StatefulWidget {
 
   static const name = 'animated_screen';
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius =10.0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +28,24 @@ class AnimatedScreen extends StatelessWidget {
       body: Center(
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
-          curve: Curves.easeOutCubic,
-          width: 300,
-          height: 140,
+          curve: Curves.bounceOut,
+          width: width,
+          height: height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20)
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius)
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          setState(() {
+            width = Random().nextInt(300) * 1.0 + 50;
+            height = Random().nextInt(300) * 1.0 + 50;
+            color = Color.fromARGB(Random().nextInt(255), Random().nextInt(255), Random().nextInt(255), Random().nextInt(255));
+            borderRadius = Random().nextInt(100) * 1.0 + 10;
+          });
+        },
         child: const Icon(Icons.play_arrow_rounded),
         ),
     );
