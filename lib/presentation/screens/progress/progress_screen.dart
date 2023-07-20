b9050f9860f4b6b38bbc94dd4885da044bae673c
Andres <andrese-rios@utp.edu.co>
2023-07-20 07:20:26 -0500

    Line and Circular progress indicator using StreamBuilder

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -36,7 +36,7 @@ class _ProgressView extends StatelessWidget {
             backgroundColor: Colors.black38,            
           ),
           SizedBox(height: 20,),
-          Text('Circular Indicator Controlado'),
+          Text('Circular Y Linear controlados'),
           SizedBox(height: 20,),
           _ControllerProgressIndicator()
         ],
@@ -50,6 +50,33 @@ class _ControllerProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder<double>(
+      stream: Stream.periodic(Duration(milliseconds: 300), (value){
+          return (value*2)/100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+
+        final progressValue = snapshot.data ?? 0;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 4,
+                backgroundColor: Colors.black12,            
+              ),
+              const SizedBox(width: 20,),
+              Expanded(            
+                child: LinearProgressIndicator(
+                  value: progressValue,
+                )
+                )
+            ],
+          ),
+          );
+      }
+    );
   }
 }
