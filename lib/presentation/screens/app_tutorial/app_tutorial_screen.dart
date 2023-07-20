be33a5a9fdee63620cf31aed3e9f755fc9726d9d
Andres <andrese-rios@utp.edu.co>
2023-07-20 11:00:38 -0500

    creacion del la pagina de tutoriales por medio de un widget PageView

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
+
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+
+final slides = <SlideInfo> [
+  SlideInfo('Busca la comida', 'Busca la comida a traves de nuestra app', 'assets/images/1.png'),
+  SlideInfo('Entrega rapida', 'accelera la entrega de tu pedido usando nuestras opciones VIP', 'assets/images/2.png'),
+  SlideInfo('Disfruta la comida', 'Buen provecho', 'assets/images/3.png'),
+];
+
+
+
 class AppTutorialScreen extends StatelessWidget {
 
   static const name = 'tutorial_screen';
@@ -7,6 +25,39 @@ class AppTutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          ...slides.map((s) => _Slide(title: s.title, caption: s.caption, imageUrl: s.imageUrl)),
+        ],
+      ),
+    );
+  }
+}
+
+
+class _Slide extends StatelessWidget {
+
+  final String title;
+  final String caption;
+  final String imageUrl;
+  const _Slide({super.key, required this.title, required this.caption, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Stack(
+        children: [
+          Image.asset(imageUrl),
+          Column(
+            children: [
+              Text(title),
+              Text(caption)
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
